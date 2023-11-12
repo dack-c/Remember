0ad53b805514988f0aad83f050695a68d277fb51
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2023-11-12 15:54:52 +0900

    show map

--- app/src/main/java/com/example/remember/NaverMapActivity.kt
+++ app/src/main/java/com/example/remember/NaverMapActivity.kt
@@ -3,12 +3,27 @@ package com.example.remember
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.remember.databinding.ActivityNaverMapBinding
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 
-class NaverMapActivity : AppCompatActivity() {
+class NaverMapActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityNaverMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
         // first commit
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map_fragment) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map_fragment, it).commit()
+            }
+
+        mapFragment.getMapAsync(this)
     }
+
+    override fun onMapReady(p0: NaverMap) {
+        //
+    }
+
 }
